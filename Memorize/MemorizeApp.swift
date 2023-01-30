180a425afd28542c34817b04e2ed642d1d378619
Roberts Kursitis <53969619+Automata02@users.noreply.github.com>
2023-01-30 08:58:47 +0200

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by roberts.kursitis on 30/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
